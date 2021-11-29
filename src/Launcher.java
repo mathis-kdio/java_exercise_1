159a953bf9cb95e41dd61b6a8fad173f01952cf3
Mathis Cadio <cadio@et.esiea.fr>
2021-11-29 15:27:47 +0100

    Partie 4 - une première commande

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,10 +6,25 @@ public class Launcher {
         boolean isEquals = false;
         do {
             Scanner sc = new Scanner(System.in);
-            String i = sc.nextLine();
-            isEquals = i.equals("quit");
-            if (!isEquals) {
-                System.out.println("Unknown command");
+            String str = sc.nextLine();
+            if (str.equals("fibo")) {
+                System.out.println("Nombre n de la suite de fibo :");
+                Scanner scFibo = new Scanner(System.in);
+                int n = Integer.parseInt(scFibo.nextLine());
+                int f0 = 0, f1 = 1, fn = 0;
+                for (int i = 1; i < n; i++) {
+                    fn = f1 + f0;
+                    f0 = f1;
+                    f1 = fn;
+                    System.out.println(fn);
+                }
+                System.out.println("A l'index f(" + n + "), la valeur de la suite de Fibonacci est " + fn);
+            }
+            else {
+                isEquals = str.equals("quit");
+                if (!isEquals) {
+                    System.out.println("Unknown command");
+                }
             }
         } while (!isEquals);
     }
