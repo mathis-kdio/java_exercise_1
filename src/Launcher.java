cc28878318aa4469233cecef82f7321ceb773359
Mathis Cadio <cadio@et.esiea.fr>
2021-11-29 15:10:24 +0100

    Partie 3 - boucle

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue");
-        Scanner sc = new Scanner(System.in);
-        String i = sc.nextLine();
-        if (i.equals("quit") == false) {
-            System.out.println("Unknown command");
-        }
+        boolean isEquals = false;
+        do {
+            Scanner sc = new Scanner(System.in);
+            String i = sc.nextLine();
+            isEquals = i.equals("quit");
+            if (!isEquals) {
+                System.out.println("Unknown command");
+            }
+        } while (!isEquals);
     }
 }
